4ef80eaa100a717be09c5e9f301130a84c9d5d7f
TanzidAnan <tanzid.anan125@gmail.com>
2025-09-19 21:01:18 +0600

    react

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import Watch from './compontent/Watch/Watch'
+
+function App() {
+
+  return (
+    <>
+      <h1>react project</h1>
+      <Watch></Watch>
+    </>
+  )
+}
+
+export default App
